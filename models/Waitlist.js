21a5f295ba79497cfc000f56710aaa5384811a6a
Mubarak Lawal <mubylaww@gmail.com>
2023-11-23 00:25:07 +0100

    main

--- models/Waitlist.js
+++ models/Waitlist.js
@@ -6,6 +6,15 @@ const WaitlistSchema = new mongoose.Schema(
       type: String,
       trim: true,
     },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
     location: {
       type: String,
       trim: true,
